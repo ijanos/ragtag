7adeb07ba71d345840a1189e6eef6a9ea2019cbb
János Illés <ijanos@gmail.com>
2011-10-31 12:45:35 +0100

    Add some crazy logging the thumbnailmaker thread
    Trying to catch some nasty segfault

--- widgets/thumbnailer.py
+++ widgets/thumbnailer.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 
+import logging
+
 from PyQt4 import *
 from PyQt4.QtCore import *
 
@@ -7,16 +9,27 @@ from PIL import Image, ImageQt
 
 class Thumbnailmaker(QRunnable):
     def __init__(self, filename, parent = None):
+        logging.debug("Starting Thumbnailmaker constructor")
         QRunnable.__init__(self)
         self.filename = filename
         # Need a QObject to emit signals from a QRunnable
         self.obj = QObject()
+        logging.debug("Starting Thumbnailmaker constructor DONE")
 
     def run(self):
+        logging.debug("Starting Thumbnailmaker RUN")
         img = Image.open(self.filename)
+
+        logging.info("creating thumbnail image")
         img.thumbnail( (200,200), Image.ANTIALIAS )
+
+        logging.info("creating thumbnail image DONE %s", img)
         thumb = ImageQt.ImageQt(img)
+
+        logging.info("creating ImageQT DONE, emitting signal %s", thumb)
         self.obj.emit(SIGNAL("imageDone"), thumb)
+
+        logging.debug("thread finished")
 
 if __name__ == "__main__":
     pass
